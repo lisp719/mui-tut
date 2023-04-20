9c147290c3da51ef6b7e95a8773c2c021369c7c8
lisp719 <368034+lisp719@users.noreply.github.com>
2023-04-21 01:53:32 +0900

    feat: Check for the pattern of TextField

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
 import { TextField } from "@mui/material";
 
+import { ChangeEvent, useState } from "react";
+
 function App() {
+  const [num, setNum] = useState("");
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.validity.patternMismatch) {
+      setNum(e.target.value);
+    }
+  };
+
   return (
     <>
-      <TextField />
+      <TextField
+        value={num}
+        onChange={handleChange}
+        inputProps={{ inputMode: "numeric", pattern: "[0-9]{1,3}" }}
+      />
     </>
   );
 }
